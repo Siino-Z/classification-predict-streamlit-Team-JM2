ba52b37f0965c6a81fc1100f38c8b71e7ff7813c
Sino Felicity Zwane <sinothaboz@gmail.com>
2023-12-13 18:04:29 +0200

    add missing package import to the streamlit_app.py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import os
 import joblib
+from scipy import sparse
 from sklearn import feature_selection
 from sklearn.datasets import make_classification
 from sklearn.feature_selection import f_classif
